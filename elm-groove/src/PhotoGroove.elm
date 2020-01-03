02cabf084610287a0e7a1ea7c28049aee33e0a12
dogwith1eye <dogwith1eye@gmail.com>
2020-01-03 12:17:14 -0500

    Added selection to Elm

--- elm-groove/src/PhotoGroove.elm
+++ elm-groove/src/PhotoGroove.elm
@@ -1,7 +1,9 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (div, figure, h1, img, section, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import List.Extra as ListE
 
 
@@ -9,36 +11,65 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
-thumbnails =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
 
 
-viewThumbnailCols thumbs =
+viewThumbnailCols selectedUrl thumbs =
     let
         column a =
-            div [ classList [ ( "column", True ), ( "is-half", List.length thumbs == 1 ) ] ] [ a ]
+            div
+                [ classList
+                    [ ( "column", True )
+                    , ( "is-half", List.length thumbs == 1 )
+                    , ( "has-background-primary", a.url == selectedUrl )
+                    ]
+                ]
+                [ viewThumbnail a ]
     in
     div [ class "columns" ]
-        (List.map (viewThumbnail >> column) thumbs)
+        (List.map column thumbs)
 
 
 viewThumbnail thumb =
     figure [ class "image is-200by267 is-marginless" ]
-        [ img [ src (urlPrefix ++ thumb.url) ] []
+        [ img
+            [ src (urlPrefix ++ thumb.url)
+            , onClick { description = "ClickedPhoto", data = thumb.url }
+            ]
+            []
         ]
 
 
 view model =
     section [ class "section" ]
-        [ div [ class "content " ]
+        [ div [ class "content" ]
             [ h1 [] [ text "Photo Groove" ]
-            , div [] (model |> ListE.greedyGroupsOf 2 |> List.map viewThumbnailCols)
+            , div [ class "columns" ]
+                [ div [ class "column" ] (model.photos |> ListE.greedyGroupsOf 2 |> List.map (viewThumbnailCols model.selectedUrl))
+                , div [ class "column" ] [ img [ src (urlPrefix ++ model.selectedUrl), class "image is-fullwidth" ] [] ]
+                ]
             ]
         ]
 
 
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+
+    else
+        model
+
+
 main =
-    view thumbnails
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
