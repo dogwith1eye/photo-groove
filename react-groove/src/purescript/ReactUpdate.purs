f3dcf3fb96c538d68c5c0cc8a355bf55bf9b29eb
dogwith1eye <dogwith1eye@gmail.com>
2020-01-22 16:34:00 -0500

    Initial useEffect

--- react-groove/src/purescript/ReactUpdate.purs
+++ react-groove/src/purescript/ReactUpdate.purs
@@ -1,18 +1,21 @@
 module ReactUpdate where
 
+
 import Prelude
 import Data.Array as Array
-import Data.Tuple(Tuple)
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Reactix as R
 
-type UpdateModel state action = { state :: state, sideEffects :: Array (Tuple state (action -> Effect Unit) -> Effect Unit) }
+type SideEffect state action = Tuple state (action -> Effect Unit) -> Effect Unit
+type UpdateModel state action = { state :: state, sideEffects :: Array (SideEffect state action) }
 
 data UpdateMsg state action
   = NoUpdate
   | Update state
-  | SideEffect (Tuple state (action -> Effect Unit) -> Effect Unit)
+  | SideEffect (SideEffect state action)
 
 reducer :: forall s a. ((s -> a -> (UpdateMsg s a))) -> (UpdateModel s a) -> a -> (UpdateModel s a)
 reducer r model msg = case (r model.state msg) of
@@ -23,4 +26,7 @@ reducer r model msg = case (r model.state msg) of
 useReducer' :: forall s a. (s -> a -> (UpdateMsg s a)) -> s -> R.Hooks (R.Reducer s a)
 useReducer' r s = do
   updModel /\ dispatch <- R.useReducer' (reducer r) { state: s, sideEffects: [] }
+  R.useEffect1' updModel.sideEffects $ do
+    results <- traverse (\f -> f (updModel.state /\ dispatch)) updModel.sideEffects
+    pure $ unit
   pure $ (updModel.state /\ dispatch)
