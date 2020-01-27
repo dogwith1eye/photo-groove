3b091264182087e2d97f43f911df65143a5035ae
dogwith1eye <dogwith1eye@gmail.com>
2020-01-27 10:48:14 -0500

    Renamed reactix-groove

--- reactix-groove/spago.dhall
+++ reactix-groove/spago.dhall
@@ -2,7 +2,7 @@
 Welcome to a Spago project!
 You can edit this file as you like.
 -}
-{ name = "my-project"
+{ name = "reactix-groove"
 , dependencies = [ "console", "effect", "psci-support", "reactix", "random" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- reactix-groove/src/index.js
+++ reactix-groove/src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import App from "./javascript/App";
-import { mkApp as App } from "./output/AppUpdate";
+import { mkApp as App } from "./output/App";
 import "./style/bulma.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
